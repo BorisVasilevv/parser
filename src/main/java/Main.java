ec32196c62f1f242543260abf81935eaea36e0be
Boris <BacR009@yandex.ru>
2023-10-03 01:44:17 +0500

    trobe;s with big data but work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,35 +13,33 @@ public class Main {
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        String query="hotdog"; //scan.next();
+        String query=scan.nextLine();
         String urlDecodeQuery=URLEncoder.encode(query, StandardCharsets.UTF_8);
         String queryToAvito = "https://www.avito.ru/all?q=" + urlDecodeQuery;
-        Integer amountElems;
-        String  userQuery;
-        //https://www.avito.ru/all?q=&p=4
-        ArrayList<Element> elements = new ArrayList<>();
+        ArrayList<Element> resultSetProducts = new ArrayList<>();
         try {
-            Document firstPage=Jsoup.connect(queryToAvito).get();
-            Elements upElems = firstPage.getElementsByClass("page-title-root-cK8oN js-page-title");
+            Integer amountElems=0;
+            Integer pageCount=2;
+            int i=0;
+            do {
+                String s =queryToAvito + String.format("&p=%d", ++i);
+                System.out.println(s);
+                Document page=Jsoup.connect(s).get();
+                Elements tempResultElems = page.getElementsByClass("iva-item-root-_lk9K photo-slider-slider-S15A_ iva-item-list-rfgcH iva-item-redesign-rop6P iva-item-responsive-_lbhG items-item-My3ih items-listItem-Gd1jN js-catalog-item-enum");
+                resultSetProducts.addAll(tempResultElems);
+                if (i==1){
+                    Elements elemCount = page.getElementsByClass("page-title-count-wQ7pG");
+                    String maxPageNumber= page.getElementsByClass("styles-module-item-kF45w styles-module-item_size_s-Tvz95 styles-module-item_last-vIJIa styles-module-item_link-_bV2N").attr("aria-label").split(" ")[1];
+                    pageCount = Integer.parseInt(maxPageNumber);
+                    amountElems = Integer.parseInt(elemCount.select("span").text().replaceAll(" ",""));
+                }
+            }while (i<=pageCount);
 
-            Elements resultElems = firstPage.getElementsByClass("iva-item-root-_lk9K photo-slider-slider-S15A_ iva-item-list-rfgcH iva-item-redesign-rop6P iva-item-responsive-_lbhG items-item-My3ih items-listItem-Gd1jN js-catalog-item-enum");
-
-            elements.addAll(resultElems);
-
-            Elements tempElems = upElems.select("div");
-            amountElems = Integer.parseInt(tempElems.select("span").text());
-            userQuery = tempElems.select("h1").text();
-            for(int i=2;elements.stream().count()<amountElems;i++){
-                Document tempPage=Jsoup.connect(queryToAvito + String.format("&p=%d", i)).get();
-
-            }
-            //System.out.println( firstPage.title());
+            System.out.printf( "Запрос: \"%s\" выдал %d результатов\n",query, amountElems);
         }
         catch (IOException e){
             System.out.println(e);
         }
-
-
-
+        System.out.println(resultSetProducts.size());
     }
 }
